a1f6d1f76bf4f3eff568a7a2a0e6a4cc51a61d22
Lex A <axelemanuelpavon@gmail.com>
2022-09-16 23:19:28 -0300

    actualizando

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routerApi = require("./routes")
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const drinksRouter = require("./drinksRouting");
 
 function routerApi(app) {
     const router = express.Router();
+    app.get("/", (req, res) => {
+        res.status(200)
+        .send("Bienvenido a esta API!");
+    })
     app.use("/api/v1", router);
     router.use("/meals", mealsRouter);
     // router.use("/categories", categoriesRouter);
