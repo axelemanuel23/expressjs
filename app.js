eb6796e344d31881f992eb8fc8b7719fb5ec38df
Lex A <axelemanuelpavon@gmail.com>
2022-10-21 00:03:04 -0300

    actualizando whitelist

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
-const whitelist = ["http://localhost:3000"];
+const whitelist = ["http://localhost:3000", "https://sensational-florentine-6e965d.netlify.app/"];
 app.use(cors({origin: whitelist}));
 
 app.use(express.json());
